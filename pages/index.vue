3bfa7984643300b59f4eb7fbd5d7b7af264e55b5
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-07-06 20:48:05 -0700

    initial commit with tailwind set up

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="text-slate-500">
+    <p class="text-3xl w-1/3">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
+      rerum. In quis quae, saepe, voluptas eveniet, odio exercitationem corporis
+      cumque rem atque necessitatibus ratione commodi. Temporibus repellendus
+      consectetur libero nihil!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
