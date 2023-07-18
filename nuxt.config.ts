4260a2a9e52bcb2c7f842acd6ff223f7a1cb9fe2
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-07-17 18:51:20 -0700

    added nuxt i, solar icon set and removed ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ["@/assets/css/main.css"],
-  modules: ["@nuxtjs/tailwindcss"],
+  //css: ["@/assets/css/main.css"],
+  modules: ["@nuxthq/ui"],
+  // content: {
+  //   highlight: {
+  //     theme: "dracula-soft",
+  //   },
+  // https://content.nuxtjs.org/api/configuration
+  //},
+  ui: { icons: ["solar"] },
+  ssr: false,
 });
