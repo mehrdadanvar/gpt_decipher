8a471738b24dad639201474e81c138e4b18bd923
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-07-17 19:01:36 -0700

    added a hero component to the index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,7 @@
 <template>
-  <div class="text-slate-500">
-    <p class="text-3xl w-1/3">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis,
-      rerum. In quis quae, saepe, voluptas eveniet, odio exercitationem corporis
-      cumque rem atque necessitatibus ratione commodi. Temporibus repellendus
-      consectetur libero nihil!
-    </p>
-  </div>
+  <main class="text-slate-500">
+    <IndexHero />
+  </main>
 </template>
 
 <script setup></script>
