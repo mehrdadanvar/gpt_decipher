d30620c9f7533dce49e97a5a767153f038eca719
mehrdad.anvar <mehrdad.anvar@gmail.com>
2023-07-17 19:17:48 -0700

    added two components for the mainpage

---
+++ components/Index/Hero.vue
@@ -0,0 +1,44 @@
+<template>
+  <div
+    class="flex flex-row w-7/12 m-auto rounded-3xl border border-slate-100 shadow-slate-300 shadow-xl"
+  >
+    <div class="p-1 w-7/12">
+      <IndexHeroPlay />
+    </div>
+    <img src="/person.jpg" alt="" class="rounded-r-3xl photo w-5/12" />
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+/* .profit {
+  background-image: url("puzzle.svg");
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 2rem;
+  height: 2rem;
+}
+.photo::before {
+  content: "";
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  height: 1500px;
+  background-color: #111827;
+  transform: skewY(3deg);
+  transform-origin: 0;
+} */
+/* .photo::after {
+  content: "";
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  height: 150px; 
+  background-color: #111827; 
+  transform: skewY(3deg);
+  transform-origin: 0;
+} */
+</style>
---
+++ components/Index/HeroPlay.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="w-5/6 rounded-lg h-auto p-4 mx-auto">
+    <div class="terminal p-2 font-Inter mb-6">
+      <div class="flex flex-col gap-12">
+        <p class="text-3xl text-teal-500">{{ rendered_text[0] }}</p>
+        <p class="ml-12 text-2xl text-slate-500">{{ rendered_text[1] }}</p>
+        <p class="ml-24 text-xl text-slate-400 shodow-2xl shadow-black">
+          {{ rendered_text[2] }}
+        </p>
+      </div>
+    </div>
+    <div class="flex flex-row gap-3 mt-24">
+      <button
+        class="bg-teal-400 text-lg text-white rounded-full w-2/5 h-12 transitionall ease-linear duration-100 hover:w-3/5"
+      >
+        How ?
+      </button>
+      <button
+        class="bg-white border border-slate-200 text-lg text-slate-700 rounded-full w-2/5 transitionall ease-linear duration-100 hover:w-3/5"
+      >
+        <NuxtLink to="register">Register</NuxtLink>
+      </button>
+    </div>
+    <!-- <p>{{ rendered_text }}</p> -->
+  </div>
+</template>
+<script setup>
+let major_text = [
+  "Assessing  GPT's Accuracy and Reliability in Answering Clinical Questions",
+  "Shall We Reject or Embrace AI?",
+  "A Scientific Answer",
+];
+let minor_text = ref("");
+
+let rendered_text = ref(["", "", ""]);
+
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
+
+async function minor_render(input, time, index) {
+  for (let i = 0; i < input.length; i++) {
+    rendered_text.value[index] += input[i];
+    await timer(time);
+  }
+}
+
+async function order(main_array) {
+  for (let i = 0; i < main_array.length; i++) {
+    await minor_render(main_array[i], 20, i);
+  }
+}
+
+async function delay() {
+  await timer(100);
+  order(major_text);
+}
+
+delay();
+</script>
+
+<style scoped>
+/* .test {
+  color: #f7e1ff;
+  text-shadow: 0 0 7px #fff, 0 0 10px #fff, 0 0 21px #fff, 0 0 42px #0fa;
+} */
+</style>
